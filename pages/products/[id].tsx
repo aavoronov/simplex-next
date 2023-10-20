c01397ec3546a26b46937a5080462b5d4674c080
whatever <voronov.xcvi@gmail.com>
2023-10-20 12:29:53 +0300

    fix

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -342,7 +342,7 @@ export default function Product({ data }: { data: DataType }) {
 }
 
 export const getStaticPaths = (async () => {
-  const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   const paths = ids.map((item) => ({ params: { id: item.toString() } }));
   return {
     paths: paths,
